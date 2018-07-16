86b7386c9d2dd78c0b3ec25f1de5a9c25c825e02
Tatsu245 <e165413m@st.u-gakugei.ac.jp>
2018-07-16 09:15:28 +0900

    eclipseと連携

---
+++ WebContent/index.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>TopPage</title>
+</head>
+<body class="text-center">
+<!-- ナビゲーションバー -->
+<nav class="navbar navbar-dark bg-dark">
+		<a class="navbar-brand" href="/tt_match_manager/">tt_match_manager</a>
+</nav>
+<br>
+<!-- コンテンツ -->
+<div class="container">
+	<h1 class="page-header">Welcome to tt_match_manager</h1>
+	<p class="lead">ようこそ，卓球戦績管理システムへ</p>
+	<br>
+	<a href="/tt_match_manager/login" class="btn btn-primary" >Get Strat</a>
+</div>
+
+</body>
+</html>
---
+++ src/controller/LoginServlet.java
@@ -0,0 +1,47 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.User;
+import service.LoginService;
+
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+    public LoginServlet() {
+    }
+    //Loginフォームの表示
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
+		dispatcher.forward(request, response);
+	}
+	//Login処理を実行
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String userId = request.getParameter("userId");
+		String password = request.getParameter("password");
+
+		LoginService loginService = new LoginService();
+
+		User user = loginService.authenticate(userId, password);
+		if(user==null) {
+			request.setAttribute("ERROR", "IDまたはパスワードが違います");
+			getServletContext().getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(request, response);
+		}else {
+			HttpSession session = request.getSession();
+			session.setAttribute("user", user);
+			response.sendRedirect("/hazelabBBS/bbs");
+		}
+	}
+
+}
