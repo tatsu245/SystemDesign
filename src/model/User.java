0b9b0fd61158420f6980ab3e3fb9befe0417097f
Tatsu245 <e165413m@st.u-gakugei.ac.jp>
2018-07-16 09:38:03 +0900

    add

---
+++ src/model/User.java
@@ -0,0 +1,47 @@
+package model;
+
+public class User {
+	private String user_id;
+	private String user_name;
+	private String password;
+	private String student_number;
+
+	public String getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getUser_name() {
+		return user_name;
+	}
+
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getStudent_number() {
+		return student_number;
+	}
+
+	public void setStudent_number(String student_number) {
+		this.student_number = student_number;
+	}
+
+	public User(String user_id, String user_name, String password, String student_number) {
+	}
+
+	public User() {
+	}
+
+}
